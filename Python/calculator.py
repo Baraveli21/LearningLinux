ad700500c36a9fcd9c4416a50d21c4d0b4c1678e
Baraveli21 <baraveliryuo@gmail.com>
2026-01-07 02:38:02 +0500

    Slight changes

---
+++ Python/calculator.py
@@ -0,0 +1,10 @@
+# This is a comment
+
+
+def addition(num1,num2):
+    print(f'The numbers to be added are {num1} and {num2}')
+    sum = num1+num2
+    print(f'The sum of {num1} and {num2} is {sum}')
+
+
+
